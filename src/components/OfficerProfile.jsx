3400a047bb5d9306b113619c4d9c3e17c2fe42d4
Chinwilla <bogdandrozd123@gmail.com>
2024-05-09 16:05:01 +0100

    updata

---
+++ src/components/OfficerProfile.jsx
@@ -0,0 +1,11 @@
+export default function OfficerProfile({ officer: {name, age, specialization, status}, }) {
+    return (
+        <div>
+            <p>Name: {name}</p>
+            <p>Age: {age} </p>
+            <p>Status: {status === 'Active' ? "Is active" : "Is retired"}</p>
+            <p>Spetialization: {specialization  } </p>
+        </div>
+    );
+    
+}
